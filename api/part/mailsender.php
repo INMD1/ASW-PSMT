3f6fdd9a628aee3055456b7f5e4ade13c2f4ab7e
INMD1 <lyw514549@gmail.com>
2025-01-01 15:58:01 +0900

    원본 소스 업데이트

---
+++ api/part/mailsender.php
@@ -0,0 +1,47 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//환경변수 로드
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+function sendGmail($to, $subject, $body, $attachments = []) {
+    require 'vendor/autoload.php';
+
+    $mail = new PHPMailer(true);
+
+    try {
+        // 서버 설정
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = $_ENV['EAMIL']; // 구글 이메일 주소
+        $mail->Password = $_ENV['APP_PASSWORD']; // 구글 앱 비밀번호
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+
+        // 발신자 설정
+        $mail->setFrom($_ENV['EAMIL'], 'Administrator');
+        
+        // 수신자 설정
+        $mail->addAddress($to);
+
+        // 첨부 파일 추가
+        foreach ($attachments as $attachment) {
+            $mail->addAttachment($attachment);
+        }
+
+        // 내용 설정
+        $mail->isHTML(true);
+        $mail->CharSet = 'UTF-8';
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        return "메일 전송 실패: {$mail->ErrorInfo}";
+    }
+}
+?>
---
+++ api/readme.md
@@ -0,0 +1,28 @@
+## 파일구조
+```
+.
+├── Server
+│   └── index.php -> 
+├── auth
+│   ├── department.php -> 유저조회 및 Auth관련 API함수 php
+│   ├── login.php -> 로그인 php
+│   ├── logout.php -> 로그아웃하는 php
+│   └── signup.php -> 가입하는 php
+├── board
+│   ├── createPost.php -> 게시판 내용을 생성하는 php
+│   └── readPost.php -> 게시판을 보기위한 php
+├── index.php -> 라우팅 역할
+└── proxmox
+    └── index.php -> Proxmox에서 API를 불려오는 php파일
+
+```
+
+## 사용기술
+
+### JWT
+저희는 회원정보를 안전하게 보관하기 위해 JWT토큰을 발급을 해서 클라이언트에셔 키를 보내서 서버에서 유저 정보를 제공하는 형식으로 운영하고 있습니다.<br/>
+그리고 JWT를 다른 방식으로 생각해서 데이터가 유출되지 않도록 하고 있습니다
+> 후드에서 살아남기 제5장 ```적보다 한 발작 앞서 나갈 것``` [예아](https://www.youtube.com/watch?v=sHW26ysdVT4)
+
+### Proxmox Rest API 
+기존 서버에서 시용하고 있는 플랫폼에서 API을 이용해서 사용했습니다.
